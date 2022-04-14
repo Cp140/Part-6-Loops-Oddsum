be9b4d62bf64c98ba175e63a07b1095887338922
Cole Palmer <PalmCole551@GOTVDSB.CA>
2022-04-14 10:57:11 -0400

    Add project files.

---
+++ Part 6 Loops, Oddsum/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace Part_6_Loops__Oddsum
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int oddSum;
+            int totalSum;
+            oddSum = 0;
+
+            Console.WriteLine("Please enter an integer:");
+            while (!Int32.TryParse(Console.ReadLine(), out oddSum))
+                Console.WriteLine("Please enter a valid number");
+
+            for (oddSum <= 0; oddSum = oddSum - 2)
+            {
+                totalSum = (totalSum + (totalSum + 2));
+
+
+
+                Console.WriteLine();
+            }
+
+
+
+
+
+
+
+
+            Console.WriteLine($"Thanks for {oddSum}!");
+        }
+    }
+}
